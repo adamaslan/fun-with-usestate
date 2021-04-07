189117f04b9cbc54ad280bbd9a253b5fcff0c006
Ellis Marte <elliscmarte@gmail.com>
2021-04-06 20:09:01 -0700

    save state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,67 @@ import MultiForm from "./Muliform";
 
 const formSections = [
   {
+    id: "receiversAddress",
     title: "Enter the receiver's address",
+    onSubmit: () => {
+      // post request
+    },
+    formFields: [
+      {
+        label: "name",
+        type: "string",
+        validation: (input) => {
+          if (!input) {
+            return "Name is required";
+          }
+          return null;
+        },
+      },
+      {
+        label: "street",
+        type: "string",
+        validation: (input) => {
+          if (!input) {
+            return "Street is required";
+          }
+          return null;
+        },
+      },
+      {
+        label: "city",
+        type: "string",
+        validation: (input) => {
+          if (!input) {
+            return "City is required";
+          }
+          return null;
+        },
+      },
+      {
+        label: "state",
+        type: "string",
+        validation: (input) => {
+          if (!input) {
+            return "State is required";
+          }
+          return null;
+        },
+      },
+      {
+        label: "zipCode",
+        type: "string",
+        validation: (input) => {
+          if (!input) {
+            return "Zip Code is required";
+          }
+          return null;
+        },
+      },
+    ],
+  },
+  {
+    id: "sendersAddress",
+    title: "Enter the sender's address",
     onSubmit: () => {
       // post request
     },
--- src/Muliform.js
+++ src/Muliform.js
@@ -4,24 +4,85 @@ import React, { useState } from "react";
 const MultiForm = (props) => {
   const [currentFormSectionIndex, setCurrentFormSectionIndex] = useState(0);
 
-  const onHandleNextSection = () =>
-    setCurrentFormSectionIndex(currentFormSectionIndex + 1);
-  const onHandlePreviousSection = () =>
-    setCurrentFormSectionIndex(currentFormSectionIndex - 1);
+  const [multiFormState, setMultiFormState] = useState(
+    props.formSections.reduce((all, formSection) => {
+      const formFieldsAsState = formSection.formFields.reduce(
+        (fields, field) => {
+          fields[field.label] = "";
+          return fields;
+        },
+        {}
+      );
+      console.log("formFieldsAsState", formFieldsAsState);
+      all[formSection.id] = formFieldsAsState;
 
+      return all;
+    }, {})
+  );
+
+  const onHandleNextSection = () => {
+    console.log(currentFormSectionIndex + 1, props.formSections.length);
+    if (currentFormSectionIndex + 1 < props.formSections.length) {
+      setCurrentFormSectionIndex(currentFormSectionIndex + 1);
+    }
+  };
+  const onHandlePreviousSection = () => {
+    if (currentFormSectionIndex > 0) {
+      setCurrentFormSectionIndex(currentFormSectionIndex - 1);
+    }
+  };
+
+  const sectionPercentOfWhole = 100 / props.formSections.length;
+  console.log("multiFormState", multiFormState);
   return (
     <div className="">
+      <div
+        className="progress-bar"
+        style={{ width: "100%", border: "1px solid black" }}
+      >
+        {props.formSections.map((section, index) => {
+          if (index < currentFormSectionIndex) {
+            return (
+              <div
+                style={{
+                  width: `${sectionPercentOfWhole}%`,
+                  backgroundColor: "blue",
+                }}
+              >
+                &nbsp;
+              </div>
+            );
+          }
+        })}
+      </div>
       <p>{props.formSections[currentFormSectionIndex].title}</p>
-      {props.formSections[currentFormSectionIndex].formFields.map(() => {
-        return <input type={field.type} id={field.label} name={field.label} />;
-
-return (
+      {props.formSections[currentFormSectionIndex].formFields.map((field) => {
+        return (
           <div>
-            <button onClick={onHandlePreviousSection}>Back</button>
-            <button onClick={onHandleNextSection}>Next</button>
+            <label for={field.label}>{field.label}</label>
+            <input
+              type={field.type}
+              id={field.label}
+              name={field.label}
+              onChange={(e) => {
+                setMultiFormState({
+                  ...multiFormState,
+                  [props.formSections[currentFormSectionIndex].id]: {
+                    ...multiFormState[
+                      props.formSections[currentFormSectionIndex].id
+                    ],
+                    [field.label]: e.target.value,
+                  },
+                });
+              }}
+            />
           </div>
         );
       })}
+      <div>
+        <button onClick={onHandlePreviousSection}>Back</button>
+        <button onClick={onHandleNextSection}>Next</button>
+      </div>
     </div>
   );
 };
