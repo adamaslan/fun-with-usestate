aa213218fc9b05838dce598e3424a50905eba822
Ellis Marte <elliscmarte@gmail.com>
2021-04-06 20:29:08 -0700

    save state (#2)

--- src/Form.js
+++ src/Form.js
@@ -4,7 +4,7 @@ import MultiForm from "./Muliform";
 const formSections = [
   {
     title: "Enter the receiver's address",
-    
+
     onSubmit: () => {
       // post request
     },
@@ -12,8 +12,64 @@ const formSections = [
       {
         label: "name",
         type: "string",
-
-        <input type="text" value={this.state.value} onSubmit={this.handleChange} />
+        validation: (input) => {
+          if (!input) {
+            return "Name is required";
+          }
+          return null;
+        },
+      },
+      {
+        label: "street",
+        type: "string",
+        validation: (input) => {
+          if (!input) {
+            return "Street is required";
+          }
+          return null;
+        },
+      },
+      {
+        label: "city",
+        type: "string",
+        validation: (input) => {
+          if (!input) {
+            return "City is required";
+          }
+          return null;
+        },
+      },
+      {
+        label: "state",
+        type: "string",
+        validation: (input) => {
+          if (!input) {
+            return "State is required";
+          }
+          return null;
+        },
+      },
+      {
+        label: "zipCode",
+        type: "string",
+        validation: (input) => {
+          if (!input) {
+            return "Zip Code is required";
+          }
+          return null;
+        },
+      },
+    ],
+  },
+  {
+    title: "Enter the sender's address",
+    onSubmit: () => {
+      // post request
+    },
+    formFields: [
+      {
+        label: "name",
+        type: "string",
         validation: (input) => {
           if (!input) {
             return "Name is required";
